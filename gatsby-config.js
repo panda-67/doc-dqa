3dffcf62d6d752806ca19bdb436e71dc84969623
Lord Arba <samsul.m@mhs.unsyiah.ac.id>
2023-01-29 17:20:33 +0700

    Update manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,15 +26,10 @@ module.exports = {
         name: "Darul Quran Aceh",
         short_name: "DQA",
         start_url: "/",
-        background_color: "#77bf43",
+        background_color: "#49a835",
         theme_color: "#dbaa27",
-        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
-        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: "standalone",
         icon: "src/images/logo.png", // This path is relative to the root of the site.
-        // An optional attribute which provides support for CORS check.
-        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
-        // Any invalid keyword or empty string defaults to `anonymous`
         crossOrigin: `use-credentials`,
       },
     },
